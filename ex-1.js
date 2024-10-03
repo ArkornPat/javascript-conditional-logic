f5a51a443b794f80cc23d696171fb1dce99bc139
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-10-03 12:55:22 +0700

    feat:add ans in ex1-ex7

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,11 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+
+if(lightBulbStatus === "On"){
+    console.log("Light bulb is On.")
+}else if(lightBulbStatus === "Off"){
+    console.log("Light bulb is Off.")
+}
+
+
