4aca298c7957ff8d3434158b229b3a39448991de
Tonkla <Apisit378819@gmail.com>
2024-10-03 15:06:32 +0700

    add ex1-ex7

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,12 @@
 //Exercise 2
 let lightBulbStatus = "On";
-
+lightBulbStatus = "Light bulb is Broken."
 // Start coding here.
+if (lightBulbStatus === "On") {
+    console.log("Light bulb is Broken.")
+} else if (lightBulbStatus === "Light bulb is Broken.") {
+    console.log("Light bulb is Broken.")
+} else {
+    console.log("Please choose the correct input (On/Off/Broken)")
+}
+   
